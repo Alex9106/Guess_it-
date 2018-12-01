d02b53f7d5779cb580a3c3ecf51903ac2ee36850
Alex Ion <36884524+Alex9106@users.noreply.github.com>
2018-12-01 17:48:25 +0200

    Add files via upload

---
+++ Guess_It.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import tkinter.messagebox
+
+
+root = Tk()
+
+
+tkinter.messagebox.showinfo('Window Title', "Gaseste username-ul si parola !")
+tkinter.messagebox.showinfo( "Window Title", "Username-ul poate fi format din: 'nick' sau 'norman', iar parola din: cifrele 0, 1 si 2 si literele 'a' si 'b'.\n Gasiti username-ul potrivit si parola potrivita pentru a debloca, contul. \n Succes!!! ")
+
+label_1 = Label(root, text='Username')
+label_2 = Label(root, text='Password')
+label_3 = Label(root)
+entry_1 = Entry(root)
+entry_2 = Entry(root)
+
+
+label_1.grid(row=0)
+label_2.grid(row=1)
+label_3.grid(row=3, column=1)
+entry_1.grid(row=0, column=1)
+entry_2.grid(row=1, column=1)
+
+
+
+def answer():
+
+    number_1 = Entry.get(entry_1)
+    number_2 = Entry.get(entry_2)
+
+    if number_1 == "nick" and number_2 == "120ba":
+        value = "Felicitari!"
+    else:
+        value = "Nu dispera. Mai incearca! :)"
+
+    print(value)
+
+
+
+button = Button(label_3, text="Check", fg="red", command=answer)
+button.pack()
+
+root.mainloop()
