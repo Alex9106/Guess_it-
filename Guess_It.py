93dd9cd220233273aa02fe79e50bbc826b1ea917
Alex Ion <36884524+Alex9106@users.noreply.github.com>
2018-12-03 17:57:43 +0200

    Update Guess_It.py

--- Guess_It.py
+++ Guess_It.py
@@ -13,13 +13,15 @@ label_2 = Label(root, text='Password')
 label_3 = Label(root)
 entry_1 = Entry(root)
 entry_2 = Entry(root)
+entry_3 = Entry(root)
+
 
 label_1.grid(row=0)
 label_2.grid(row=1)
 label_3.grid(row=3, column=1)
 entry_1.grid(row=0, column=1)
 entry_2.grid(row=1, column=1)
-
+entry_3.grid(row=2, column=1)
 
 
 def answer():
@@ -27,15 +29,15 @@ def answer():
     number_1 = Entry.get(entry_1)
     number_2 = Entry.get(entry_2)
 
+
     if number_1 == "nick" and number_2 == "120ba":
         value = "Felicitari!"
     elif number_1 == "nick" or number_2 == "120ba":
         value = "Ai nimerit una. Inca putin!"
     else:
         value = "Nu dispera. Mai incearca! :)"
-
-    print(value)
-
+        
+    Entry.insert(entry_3, 0 , value)
 
 
 button = Button(label_3, text="Check", fg="red", command=answer)
